54e916534600898ccda73ccb67af46848246d44b
Harry Kim <hkim@securitycompass.com>
2020-05-22 11:38:09 -0400

    Change to RTM API

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import ssl as ssl_lib
 import certifi
 from tutorial import OnboardingTutorial
 from foosboi import *
+from slack import RTMClient
+from slack.errors import SlackApiError
 from slackeventsapi import SlackEventAdapter
 from local_settings import *
 from sqlalchemy.orm import sessionmaker
@@ -21,10 +23,16 @@ onboarding_tutorials_sent = {}
 
 def command(f):
     def wrapped_function(*args, **kwargs):
-        message = f(*args, **kwargs)  # func
-        web_client = args[0]
-        channel = args[1]
-        web_client.chat_postMessage(channel=channel, text=message)
+        try:
+            message = f(*args, **kwargs)  # func
+            web_client = args[0]
+            channel = args[1]
+            web_client.chat_postMessage(channel=channel, text=message)
+        except SlackApiError as e:
+            # You will get a SlackApiError if "ok" is False
+            assert e.response["ok"] is False
+            assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+            print(f"Got an error: {e.response['error']}")
     return wrapped_function
 
 @command
@@ -34,7 +42,7 @@ def start_game(web_client: slack.WebClient, channel: str, user_id: str):
 
     # Get the onboarding message payload
     #message = onboarding_tutorial.get_message_payload()
-    user = client.users_info(user=user_id)
+    user = web_client.users_info(user=user_id)
     message = foosboi.start_game(players_info=[user])
     #message = foosboi.get_message_payload()
 
@@ -45,7 +53,7 @@ def start_game(web_client: slack.WebClient, channel: str, user_id: str):
     # we can use it to update the message after a user
     # has completed an onboarding task.
     #onboarding_tutorial.timestamp = response["ts"]
-    foosboi.timestamp = response["ts"]
+    #foosboi.timestamp = response["ts"]
 
     # Store the message sent in onboarding_tutorials_sent
     if channel not in onboarding_tutorials_sent:
@@ -57,9 +65,10 @@ def start_game(web_client: slack.WebClient, channel: str, user_id: str):
 @command
 def add_players(web_client: slack.WebClient, channel: str, players: List[dict]):
     users = []
+    import pdb; pdb.set_trace()
     for user_id in players:
         user_id = user_id.strip('<@>')
-        users.append(client.users_info(user=user_id))
+        users.append(web_client.users_info(user=user_id))
 
     return foosboi.add_players(players_info=users)
 
@@ -95,13 +104,14 @@ def history(web_client: slack.WebClient, channel: str, user:str, num_games:int):
 @command
 def balance(web_client: slack.WebClient, channel: str, user_id:str):
     user_id = user_id.strip('<@>')
-    user = client.users_info(user=user_id)
+    user = web_client.users_info(user=user_id)
     return foosboi.get_balance(user)
 
 @command
 def rebuy(web_client: slack.WebClient, channel: str, user_id:str):
     user = client.users_info(user=user_id)
     return foosboi.rebuy(user)
+
 # ================ Team Join Event =============== #
 # When the user first joins a team, the type of the event will be 'team_join'.
 # Here we'll link the onboarding_message callback to the 'team_join' event.
@@ -191,23 +201,72 @@ def update_pin(**payload):
 # Here we'll link the message callback to the 'message' event.
 @slack.RTMClient.run_on(event="message")
 def message(**payload):
-    """Display the onboarding welcome message after receiving a message
-    that contains "start".
+    """
+    Handle commands
     """
     data = payload["data"]
-    web_client = payload["web_client"]
+    client = payload["web_client"]
     channel_id = data.get("channel")
     user_id = data.get("user")
+    thread_ts = data.get("thread_ts")
     text = data.get("text")
 
-    if text and text.lower() == "start":
-        return start_onboarding(web_client, user_id, channel_id)
-
-
-@slack_events_adapter.on("button")
-def button_pressed(event_data):
-    print("BUTTON PRESSED")
-
+    try:
+        if "start" in text:
+            response = start_game(client, channel_id, user_id)
+        elif "games" in text:
+            response = games(client, channel_id, user_id)
+        elif "join" in text:
+            response = add_players(client, channel_id, [user_id])
+        elif "add player" in text:
+            players = text.split()[2:]
+            response = add_players(client, channel_id, players)
+        elif "cancel all" in text:
+            response = cancel_all_games(client, channel_id)
+        elif "cancel game" in text:
+            message_list = text.split()
+            game_num = int(message_list[2]) if len(message_list) > 2 else 0
+            response = cancel_game(client, channel_id, game_num)
+        elif "finish game" in text:
+            score = text.split()[2:][0]
+            response = finish_game(client, channel_id, score)
+        elif "stats" in text:
+            response = stats(client, channel_id)
+        elif "shuffle" in text:
+            message_list = text.split()
+            game_num = int(message_list[2]) if len(message_list) > 2 else 0
+            response = shuffle(client, channel_id, game_num)
+        elif "history" in text:
+            message_list = text.split()
+            user = message_list[1]
+            game_num = message_list[2] if len(message_list) > 2 else 5
+            game_num = -1 if game_num == "all" else int(game_num)
+            response = history(client, channel_id, user, game_num)
+        elif "balance" in text:
+            message_list = text.split()
+            try:
+                user = message_list[1]
+            except IndexError:
+                user = user_id
+            response = balance(client, channel_id, user)
+        elif "rebuy" in text:
+            message_list = text.split()
+            user = user_id
+            response = rebuy(client, channel_id, user)
+    except Exception as e:
+        print(e)
+        
+#    try:
+#        response = web_client.chat_postMessage(
+#            channel=channel_id,
+#            text=response,
+#            thread_ts=thread_ts
+#        )
+#    except SlackApiError as e:
+#        # You will get a SlackApiError if "ok" is False
+#        assert e.response["ok"] is False
+#        assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+#        print(f"Got an error: {e.response['error']}")
 
 @slack_events_adapter.on("message")
 def handle_message(event_data):
@@ -264,15 +323,15 @@ logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler())
 ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
 slack_token = SLACK_BOT_TOKEN
-#rtm_client = slack.RTMClient(token=slack_token, ssl=ssl_context)
-#rtm_client.start()
 foosboi = Foosboi()
-client = slack.WebClient(token=slack_token)
+rtm_client = slack.RTMClient(token=slack_token, ssl=ssl_context)
+rtm_client.start()
+#client = slack.WebClient(token=slack_token)
 
-response = client.chat_postMessage(
-    channel='#bottesters',
-    text="Hello world!")
-assert response["ok"]
-assert response["message"]["text"] == "Hello world!"
-slack_events_adapter.start(port=3000, debug=True)
+#response = client.chat_postMessage(
+#    channel='#bottesters',
+#    text="Hello world!")
+#assert response["ok"]
+#assert response["message"]["text"] == "Hello world!"
+#slack_events_adapter.start(port=3000, debug=True)
 
